47aab83599464b0608fa866e954d0c3f78838ebb
Sergey Ososov <helloimclinker@gmail.com>
2022-11-29 16:42:53 +0700

    + for bellman test. It's done

--- searches/ford_bellman.cpp
+++ searches/ford_bellman.cpp
@@ -11,7 +11,7 @@ void search_ford_bellman(vector<Edge> edges, int n, int m, int start_vertex) {
     d[start_vertex] = 0;
     for (int i = 0; i < n - 1; ++i) {
         for (int j = 0; j < m; ++j) {
-            if (d[edges[j].get_prev()] < INF) {
+            if (d[edges[j].get_prev()] != INF) {
                 d[edges[j].get_next()] = min (d[edges[j].get_next()], d[edges[j].get_prev()] + edges[j].get_cost());
             }
         }
@@ -25,18 +25,49 @@ void search_ford_bellman(vector<Edge> edges, int n, int m, int start_vertex) {
 
 void do_fbel_test() {
     vector<Edge> t;
-    int n = 5;
-    int m = 5;
-    Edge t1(0, 1, 10), t2(1, 2, 5), t3(2, 4, 7), t4(2, 3, 1), t5(3, 4, 2);
-    cout << "from: " << t1.get_prev() << " to: " << t1.get_next() << " cost: " << t1.get_cost() << "\n";
-    cout << "from: " << t2.get_prev() << " to: " << t2.get_next() << " cost: " << t2.get_cost() << "\n";
-    cout << "from: " << t3.get_prev() << " to: " << t3.get_next() << " cost: " << t3.get_cost() << "\n";
-    cout << "from: " << t4.get_prev() << " to: " << t4.get_next() << " cost: " << t4.get_cost() << "\n";
-    cout << "from: " << t5.get_prev() << " to: " << t5.get_next() << " cost: " << t5.get_cost() << "\n";
-    t.push_back(t1);
-    t.push_back(t2);
-    t.push_back(t3);
-    t.push_back(t4);
-    t.push_back(t5);
+    int n = 6;
+    int m = 13;
+
+    t.push_back(Edge(0, 0, 0));
+    t.push_back(Edge(0, 1, 2));
+    t.push_back(Edge(0, 2, INF));
+    t.push_back(Edge(0, 3, -4));
+    t.push_back(Edge(0, 4, 1));
+    t.push_back(Edge(0, 5, INF));
+
+    t.push_back(Edge(1, 0, INF));
+    t.push_back(Edge(1, 1, 0));
+    t.push_back(Edge(1, 2, -4));
+    t.push_back(Edge(1, 3, INF));
+    t.push_back(Edge(1, 4, INF));
+    t.push_back(Edge(1, 5, -1));
+
+    t.push_back(Edge(2, 0, INF));
+    t.push_back(Edge(2, 1, -4));
+    t.push_back(Edge(2, 2, 0));
+    t.push_back(Edge(2, 3, INF));
+    t.push_back(Edge(2, 4, 7));
+    t.push_back(Edge(2, 5, INF));
+
+    t.push_back(Edge(3, 0, INF));
+    t.push_back(Edge(3, 1, INF));
+    t.push_back(Edge(3, 2, -8));
+    t.push_back(Edge(3, 3, 0));
+    t.push_back(Edge(3, 4, 5));
+    t.push_back(Edge(3, 5, INF));
+
+    t.push_back(Edge(4, 0, INF));
+    t.push_back(Edge(4, 1, 7));
+    t.push_back(Edge(4, 2, INF));
+    t.push_back(Edge(4, 3, 4));
+    t.push_back(Edge(4, 4, 0));
+    t.push_back(Edge(4, 5, INF));
+
+    t.push_back(Edge(5, 0, 2));
+    t.push_back(Edge(5, 1, INF));
+    t.push_back(Edge(5, 2, INF));
+    t.push_back(Edge(5, 3, 7));
+    t.push_back(Edge(5, 4, -8));
+    t.push_back(Edge(5, 5, 0));
     search_ford_bellman(t, n, m, 0);
 }
